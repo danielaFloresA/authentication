489b9df2be5409fd182e71fd51769262dbf0ef67
juan pablo mejia <danielafloresavila.02@gmail.com>
2019-08-29 21:56:10 -0400

    implementacion de login
    implementacion de login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {user : null};
+    this.iniciarSesion = this.iniciarSesion.bind(this);
+    this.cerrarSesion =this.cerrarSesion.bind(this);
+  
+  }
+    componentWillMount(){
+     firebase.auth().onAuthStateChanged((user) => {
+       if(user){
+         console.log('Existe el email ', user);
+       } else {
+         console.log('no existe el email ');
+       }
+       this.setState({user:user});
+     });
+    } 
+
+    iniciarSesion(){
+
+      const provider = new firebase.auth.FacebookAuthProvider()
+      
+      firebase.auth().signInWithPopup(provider)
+        .then(result => console.log(`${result.user.email} a inciado sesion`))
+        .catch(error => console.log(`Error ${error.code}:${error.message}`))
+    }
+   
+    cerrarSesion(){
+      firebase.auth().signOut()
+      .then(result => console.log("a cerrado sesion"))
+      .catch(error => console.log(`${error.code}: ${error.message}`))
+    }
+    render(){
+      return (
+        <div>
+          <div>
+          {(this.state.user) ? <button onClick={this.cerrarSesion}>Cerrar Sesion</button> : <button onClick={this.iniciarSesion}> Iniciar sesion </button>}
+          </div>
+          
+          <h4>{(this.state.user) ? this.state.user.displayName : 'no existe'}</h4> 
+          {(this.state.user) ? <img with='32' src={this.state.user.photoURL}/> : ''}
+
+        </div>
+      )
+    }
 }
 
 export default App;
