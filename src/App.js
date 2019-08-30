df1d5af79a368a0ae5752303621625872abb0570
juan pablo mejia <danielafloresavila.02@gmail.com>
2019-08-30 08:11:01 -0400

    implementacion de la app
    implementacion de la app

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class  App extends React.Component {
     this.state = {user : null};
     this.iniciarSesion = this.iniciarSesion.bind(this);
     this.cerrarSesion =this.cerrarSesion.bind(this);
+    this.listaDeJugadores = this.listaDeJugadores.bind(this);
   
   }
     componentWillMount(){
      firebase.auth().onAuthStateChanged((user) => {
        if(user){
+        let idIngresado = user.email;
+        firebase.firestore().collection('jugadores').doc(idIngresado).set(
+        {
+          email: idIngresado, 
+          valorActual:1,
+          nombre: user.displayName,
+          estado : true
+        });
          console.log('Existe el email ', user);
        } else {
          console.log('no existe el email ');
@@ -26,24 +35,35 @@ class  App extends React.Component {
       const provider = new firebase.auth.FacebookAuthProvider()
       
       firebase.auth().signInWithPopup(provider)
-        .then(result => console.log(`${result.user.email} a inciado sesion`))
+        .then(result => {
+          
+        })
         .catch(error => console.log(`Error ${error.code}:${error.message}`))
     }
    
     cerrarSesion(){
       firebase.auth().signOut()
-      .then(result => console.log("a cerrado sesion"))
+      .then(result => {
+          firebase.firestore().collection('jugadores').doc(this.state.user.email).update({estado : false})
+        }   
+      )
       .catch(error => console.log(`${error.code}: ${error.message}`))
     }
+    listaDeJugadores(){
+      firebase.firestore().collection('jugadores').doc('id').onSnapshot(onSnapshot => 
+      console.log(onSnapshot)
+      )
+     }
     render(){
       return (
         <div>
           <div>
           {(this.state.user) ? <button onClick={this.cerrarSesion}>Cerrar Sesion</button> : <button onClick={this.iniciarSesion}> Iniciar sesion </button>}
           </div>
-          
           <h4>{(this.state.user) ? this.state.user.displayName : 'no existe'}</h4> 
           {(this.state.user) ? <img with='32' src={this.state.user.photoURL}/> : ''}
+          <button onClick={this.existeCollection}>existecollection</button>
+          <button onClick={this.listaDeJugadores}>Lista de jugadores</button>
 
         </div>
       )
